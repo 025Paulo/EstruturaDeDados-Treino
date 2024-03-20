6ef0cf50a75c60649c27a3fc15e41bb07ad4bab2
025Paulo <paulo.batista025@outlook.com>
2024-03-20 17:29:03 -0300

    treino buscar vetor

--- src/vetor/DBuscarElemento.java
+++ src/vetor/DBuscarElemento.java
@@ -1,39 +1,37 @@
 package vetor;
 
+import java.util.Scanner;
+
 // Buscar elemento em um vetor pelo Valor
 public class DBuscarElemento {
 
     public static void main(String[] args) {
 
-        int[] vetor = { 1, 2, 3, 4, 5, 6};
-
-        int valorQueEuQueroBuscar = 4;
-
-        for (int i=0;i<vetor.length;i++) {
-            if (vetor[i] == valorQueEuQueroBuscar) {
-                System.out.println("Está na posição: " + i);
-            }
-        }
+//        int[] vetor = {1, 2, 3, 4, 5, 6};
+//
+//        int valorQueEuQueroBuscar = 4;
+//
+//        for (int i = 0; i < vetor.length; i++) {
+//            if (vetor[i] == valorQueEuQueroBuscar) {
+//                System.out.println("Está na posição: " + (i + 1));
+//            }
+//        }
 
         // Transformar em uma função.
         // Função recebe como parametro o Vetor e o valor a ser buscado.
         // Função retorna a posição do valor no vetor (índice)
 
-        int[] vetor2 = { 1, 2, 3, 4, 5, 6};
 
-        vetor2[1] = 3;
-        vetor2[2] = 6;
-        vetor2[3] = 9;
-        vetor2[4] = 12;
-        vetor2[5] = 15;
-        vetor2[6] = 18;
+        int[] vetor2 = {2, 4, 8, 16, 32, 64};
 
-        int Busca = 4;
-        int Posicao = 0;
+        int busca = 16;
 
-        for (int i=0;i<vetor.length;i++) {
-            if (vetor[i] == Busca) {
+        for (int u = 0; u < vetor2.length; u++) {
+            if (vetor2[u] == busca) {
+                System.out.println("O número " + busca + " está na posição: " + (u + 1));
+            }
 
+        }
     }
-
 }
+
